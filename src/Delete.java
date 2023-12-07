7c13aae99d088266494428b3c6d30fe411a6bfc1
Fauzan Ramadhana Sadikin <fauzanramadhana6@gmail.com>
2023-12-07 22:04:27 +0700

    Bismillah gacor

---
+++ src/Delete.java
@@ -0,0 +1,34 @@
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author fauzanramadhana
+ */
+public class Delete {
+
+    public static void main(String[] args) {
+        try {
+            try (Connection conn = Koneksi.getConnection()) {
+
+                String kueriInsert = "DELETE FROM users WHERE id = ?";
+                PreparedStatement ps = conn.prepareStatement(kueriInsert);
+
+                ps.setInt(1, 11);
+                int rowAffected = ps.executeUpdate();
+                if (rowAffected > 0) {
+                    System.out.println("Delete data berhasil");
+                }
+                ps.close();
+            }
+        } catch (SQLException ex) {
+            System.out.println("Eksepsi akses data: " + ex.getMessage());
+        }
+    }
+}
---
+++ src/Koneksi.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author fauzanramadhana
+ */
+import com.mysql.cj.jdbc.MysqlDataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class Koneksi {
+    private static final String DB_URL = "jdbc:mysql://localhost:3308/RKPay";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "";
+
+    public static Connection getConnection() {
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setUrl(DB_URL);
+        dataSource.setUser(DB_USERNAME);
+        dataSource.setPassword(DB_PASSWORD);
+
+        try {
+            return dataSource.getConnection();
+        } catch (SQLException ex) {
+            System.out.println("Eksepsi akses data: " + ex.getMessage());
+            throw new RuntimeException("Gagal mendapatkan koneksi database.", ex);
+        }
+    }
+}
